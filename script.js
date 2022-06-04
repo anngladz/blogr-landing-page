b301030bdf8290645ca8bda126070cab9f8f6cf1
anngladz <agladzinska@gmail.com>
2022-06-04 17:39:56 +0200

    Fix mobile menu

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu-wrap");
+const dropdown = document.querySelectorAll(".menu-wrap>ul>li");
+console.log(dropdown);
+
+dropdown.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.querySelector("ul").classList.toggle("active");
+    link.querySelector("span").classList.toggle("rotate");
+  });
+});
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
